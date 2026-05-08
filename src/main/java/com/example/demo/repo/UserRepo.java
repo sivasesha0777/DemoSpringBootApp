01268841403ffffac7dab3b162e00245e0220406
sivaseshareddy <sivasesha0777@gmail.com>
2026-05-08 16:46:36 +0530

    In User Repo Method Created

--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -1,7 +1,9 @@
 package com.example.demo.repo;
 
+import com.example.demo.entity.Employee;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface UserRepo extends JpaRepository<User, Long> {
+    List<Employee> findByDepartmentId(Long departmentId);
 }
